3fe440192d674760a4b32e0daaec09dbd1d46503
deeks-13 <dylankay2022@gmail.com>
2026-04-29 15:31:32 -0500

    Refactor imports and add DataFrameImputer class

--- Portfolio/StreamlitApp_Project.py
+++ Portfolio/StreamlitApp_Project.py
@@ -39,7 +39,8 @@ from Custom_Classes import (
     OneHotEncodeCats,
     LoanFeatureEngineer,
     AutoPowerTransformer,
-    FeatureSelector
+    FeatureSelector,
+    DataFrameImputer
 )
 
 # ── Load Dataset ──────────────────────────────────────────────────────────────
