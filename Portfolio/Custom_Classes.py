09d9bfad72964d6559f029db9eb2a2141048846f
deeks-13 <dylankay2022@gmail.com>
2026-04-29 11:31:10 -0500

    Comment out Word2Vec import in Custom_Classes.py
    Comment out the import statement for Word2Vec.

--- Portfolio/Custom_Classes.py
+++ Portfolio/Custom_Classes.py
@@ -4,7 +4,7 @@ import statsmodels.api as sm
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import PowerTransformer
 from scipy.stats import skew
-from gensim.models import Word2Vec
+# from gensim.models import Word2Vec
 
 
 
